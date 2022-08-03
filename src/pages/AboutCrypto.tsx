5a232b181e0b1fdf24de246ca7ae3774e0714673
denis <denismatysik@gmail.com>
2022-08-03 21:30:40 +0300

    fix about page

--- src/pages/AboutCrypto.tsx
+++ src/pages/AboutCrypto.tsx
@@ -1,9 +1,4 @@
-import React from "react";
-import { useSelector } from "react-redux";
-
 export default function AboutCrypto() {
-  const check: any = useSelector((state: any) => state.crypto.cryptoAll);
-
   return (
     <div>
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio,
