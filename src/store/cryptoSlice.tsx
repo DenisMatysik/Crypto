497ded224a6320ca22b05f1e7c1498a00c075709
denis <denismatysik@gmail.com>
2022-08-04 23:24:28 +0300

    fix

--- src/store/cryptoSlice.tsx
+++ src/store/cryptoSlice.tsx
@@ -1,34 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { cryptos, cryptosPortfolio } from "../data/cryptos";
 
 const getInitialCryptos = () => {
   const localCryptoList = window.localStorage.getItem("cryptoList");
   if (localCryptoList) {
     return JSON.parse(localCryptoList);
   }
-  window.localStorage.setItem(
-    "cryptoList",
-    JSON.stringify([
-      {
-        cryptoInf: {
-          id: "bitcoin",
-          rank: "1",
-          symbol: "BTC",
-          name: "Bitcoin",
-          supply: "19110256.0000000000000000",
-          maxSupply: "21000000.0000000000000000",
-          marketCapUsd: "438337851832.0349102147690064",
-          volumeUsd24Hr: "11325632332.5571368746723394",
-          priceUsd: "22937.3092559322549219",
-          changePercent24Hr: "-0.8895917257909614",
-          vwap24Hr: "23062.7554814613343880",
-          explorer: "https://blockchain.info/",
-        },
-        id: "Bitcoin",
-        inputInf: "123",
-        time: "04.08.2022, 23:01:59",
-      },
-    ])
-  );
+  window.localStorage.setItem("cryptoList", JSON.stringify(cryptosPortfolio));
   return [];
 };
 
@@ -37,53 +15,7 @@ const getAllCryptos = () => {
   if (allCryptos) {
     return JSON.parse(allCryptos);
   }
-  window.localStorage.setItem(
-    "AllCryptos",
-    JSON.stringify([
-      {
-        id: "bitcoin",
-        rank: "1",
-        symbol: "BTC",
-        name: "Bitcoin",
-        supply: "19110256.0000000000000000",
-        maxSupply: "21000000.0000000000000000",
-        marketCapUsd: "438337851832.0349102147690064",
-        volumeUsd24Hr: "11325632332.5571368746723394",
-        priceUsd: "22937.3092559322549219",
-        changePercent24Hr: "-0.8895917257909614",
-        vwap24Hr: "23062.7554814613343880",
-        explorer: "https://blockchain.info/",
-      },
-      {
-        id: "bitcoin",
-        rank: "1",
-        symbol: "BTC",
-        name: "Bitcoin",
-        supply: "19110256.0000000000000000",
-        maxSupply: "21000000.0000000000000000",
-        marketCapUsd: "438337851832.0349102147690064",
-        volumeUsd24Hr: "11325632332.5571368746723394",
-        priceUsd: "22937.3092559322549219",
-        changePercent24Hr: "-0.8895917257909614",
-        vwap24Hr: "23062.7554814613343880",
-        explorer: "https://blockchain.info/",
-      },
-      {
-        id: "bitcoin",
-        rank: "1",
-        symbol: "BTC",
-        name: "Bitcoin",
-        supply: "19110256.0000000000000000",
-        maxSupply: "21000000.0000000000000000",
-        marketCapUsd: "438337851832.0349102147690064",
-        volumeUsd24Hr: "11325632332.5571368746723394",
-        priceUsd: "22937.3092559322549219",
-        changePercent24Hr: "-0.8895917257909614",
-        vwap24Hr: "23062.7554814613343880",
-        explorer: "https://blockchain.info/",
-      },
-    ])
-  );
+  window.localStorage.setItem("AllCryptos", JSON.stringify(cryptos));
   return [];
 };
 
