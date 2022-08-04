4319d8645b0ff984ad55e2315f39fb66708a8a29
denis <denismatysik@gmail.com>
2022-08-04 21:58:28 +0300

    fix

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,6 @@
 import Header from "../components/header/Header";
 import Pagination from "../components/pagination/Pagination";
 import PaginationBtns from "../components/pagination/PaginationBtns";
-import { store } from "../store/store";
-import { Provider } from "react-redux";
 
 export default function Main() {
   return (
