cdee24b8efcde0ecb34605db9f5df3f09a214dd5
denis <denismatysik@gmail.com>
2022-08-04 23:50:07 +0300

    header fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ModalPortfolio from "../modal/ModalPortfolio";
 import "./Header.scss";
+import { addAllCryptos } from "../../store/cryptoSlice";
+import { cryptos } from "../../data/cryptos";
 
 export default function Header() {
   const [openPortfolio, setOpenPortfolio] = useState(false);
   const inf = useSelector((state: any) => state.crypto.cryptoList);
   const allInf = useSelector((state: any) => state.crypto.allCryptos);
+  const [allCryptos, setAllCryptos] = useState(cryptos);
+  const dispatch = useDispatch();
 
   const updatingArr: any = [];
   for (const item of allInf) {
@@ -34,41 +38,50 @@ export default function Header() {
     0
   );
 
-  if (allInf) {
-    return <div>Empty</div>;
-  } else
-    return (
-      <header>
-        <div>
-          Top 3 ratting cryptos : {} <span>{allInf[0].name}</span>,{" "}
-          <span>{allInf[1].name}</span>,<span>{allInf[2].name}</span>
+  async function fetctCryptos() {
+    try {
+      const response = await fetch("https://api.coincap.io/v2/assets");
+      let data = await response.json();
+      setAllCryptos(data.data);
+      dispatch(addAllCryptos(data.data));
+    } catch (error) {
+      console.log("Fetch error: ", error);
+    }
+  }
+
+  useEffect(() => {
+    fetctCryptos();
+  }, []);
+
+  return (
+    <header>
+      <div>
+        Top 3 ratting cryptos : {} <span>{allCryptos[0].name}</span>,{" "}
+        <span>{allCryptos[1].name}</span>,<span>{allCryptos[2].name}</span>
+      </div>
+      <div className="portfolio">
+        <div className="inf">
+          <span>Total:{updatePricePortfolio.toFixed(3)}$</span>
+          <span>
+            Difference:{(oldPricePortfolio - updatePricePortfolio).toFixed(3)}$
+            (
+            {(
+              ((oldPricePortfolio - updatePricePortfolio) / oldPricePortfolio) *
+              100
+            ).toFixed(3)}
+            %)
+          </span>
         </div>
-        <div className="portfolio">
-          <div className="inf">
-            <span>Total:{updatePricePortfolio.toFixed(3)}$</span>
-            <span>
-              Difference:{(oldPricePortfolio - updatePricePortfolio).toFixed(3)}
-              $ (
-              {(
-                ((oldPricePortfolio - updatePricePortfolio) /
-                  oldPricePortfolio) *
-                100
-              ).toFixed(3)}
-              %)
-            </span>
-          </div>
-          <img
-            className="portfolio_img"
-            src="http://cdn.onlinewebfonts.com/svg/img_543533.png"
-            alt="you_portfolio"
-            onClick={() => {
-              setOpenPortfolio(true);
-            }}
-          />
-        </div>
-        {openPortfolio && (
-          <ModalPortfolio setOpenPortfolio={setOpenPortfolio} />
-        )}
-      </header>
-    );
+        <img
+          className="portfolio_img"
+          src="http://cdn.onlinewebfonts.com/svg/img_543533.png"
+          alt="you_portfolio"
+          onClick={() => {
+            setOpenPortfolio(true);
+          }}
+        />
+      </div>
+      {openPortfolio && <ModalPortfolio setOpenPortfolio={setOpenPortfolio} />}
+    </header>
+  );
 }
